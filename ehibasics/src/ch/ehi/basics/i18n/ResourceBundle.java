564f7b9143b3cbcee88bb8a82ef3d8e98e5239f8
claeis <claeis@users.noreply.github.com>
2004-01-28 07:44:48 +0000

    getImageIcon() added
    git-svn-id: file:///C/usr/ce/projects/sf/ili2c/gitmigration/umleditor.svn/umleditor/trunk@50 22a1f05a-e80c-4436-89a7-166f12dbcbdd

--- ehibasics/src/ch/ehi/basics/i18n/ResourceBundle.java
+++ ehibasics/src/ch/ehi/basics/i18n/ResourceBundle.java
@@ -1,10 +1,40 @@
 package ch.ehi.basics.i18n;
 
 public class ResourceBundle {
-
+	private final static String RESOURCES_DIRECTORY = "resources";
   private ResourceBundle() {
   }
+  /** Get the appropriate ResourceBundle.
+   * uses transformName to get the right bundle name.
+   */
+  public static java.util.ResourceBundle getBundle(Class baseClass){
+    return java.util.ResourceBundle.getBundle(transformName(baseClass.getName()));
+  }
+  /** 
+   * Get the appropriate ResourceBundle for a given Locale.
+   * Uses transformName() to get the right bundle name.
+   */
+  public static java.util.ResourceBundle getBundle(java.lang.Class baseClass, java.util.Locale locale) {
+    return java.util.ResourceBundle.getBundle(transformName(baseClass.getName()), locale);
+  }
+/**
+ * Return an ImageIcon in a Package named "resources" relative to
+ * the given Class' path.
+ * @return ImageIcon
+ */
+public static javax.swing.ImageIcon getImageIcon(Class aClass, String imageFileName) {
+	String className = aClass.getName();
+	int index = className.lastIndexOf('.');
+	String file = className.substring(0, index).replace('.', '/') + "/" + RESOURCES_DIRECTORY + "/" + imageFileName;	
+	
+	// var I) from IDE with relative FileSystem or compiled within Jar
+	java.net.URL url = aClass.getResource("/" + file);
 
+	// var II) from JAR with External FileSystem
+	//	java.net.URL url = getClass().getResource(file);
+
+	return new javax.swing.ImageIcon(url);
+}
   /** transform a class name into a corresponding resource bundle name.
    * insert 'resources' into the given fully qualified classname.
    * @param 'ch.ehi.Text'
@@ -12,13 +42,7 @@ public class ResourceBundle {
    */
   public static String transformName(String className) {
     StringBuffer resourceName = new StringBuffer(className);
-    resourceName.insert(className.lastIndexOf('.'),".resources");
+    resourceName.insert(className.lastIndexOf('.'), "." + RESOURCES_DIRECTORY);
     return resourceName.toString();
-  }
-  /** Get the appropriate ResourceBundle.
-   * uses transformName to get the right bundle name.
-   */
-  public static java.util.ResourceBundle getBundle(Class aclass){
-    return java.util.ResourceBundle.getBundle(transformName(aclass.getName()));
   }
 }
