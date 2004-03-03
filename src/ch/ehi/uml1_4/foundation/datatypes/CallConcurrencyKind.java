054b909eddaa0fbb9dd997a76480c934a3967212
claeis <claeis@users.noreply.github.com>
2004-03-03 07:58:00 +0000

    enumeration values added to support operations.
    git-svn-id: file:///C/usr/ce/projects/sf/ili2c/gitmigration/umleditor.svn/umleditor/trunk@86 22a1f05a-e80c-4436-89a7-166f12dbcbdd

--- src/ch/ehi/uml1_4/foundation/datatypes/CallConcurrencyKind.java
+++ src/ch/ehi/uml1_4/foundation/datatypes/CallConcurrencyKind.java
@@ -1,7 +1,7 @@
 // Copyright (c) 2002, Eisenhut Informatik
 // All rights reserved.
-// $Date: 2003-12-23 10:37:43 $
-// $Revision: 1.1.1.1 $
+// $Date: 2004-03-03 07:58:00 $
+// $Revision: 1.2 $
 //
 
 // -beg- preserve=no 33F0EC2002BC package "CallConcurrencyKind"
@@ -18,8 +18,9 @@ package ch.ehi.uml1_4.foundation.datatypes;
 
 // -end- 33F0EC2002BC import "CallConcurrencyKind"
 
-/** @author Claude Eisenhut
- *  @version $Revision: 1.1.1.1 $ $Date: 2003-12-23 10:37:43 $
+/** An enumeration that denotes the semantics of multiple concurrent calls to the same passiveinstance (i.
+ *  @author Claude Eisenhut
+ *  @version $Revision: 1.2 $ $Date: 2004-03-03 07:58:00 $
  */
 public interface CallConcurrencyKind extends java.io.Serializable
 {
@@ -28,6 +29,24 @@ public interface CallConcurrencyKind extends java.io.Serializable
   // -beg- preserve=no 33F0EC2002BC detail_begin "CallConcurrencyKind"
 
   // -end- 33F0EC2002BC detail_begin "CallConcurrencyKind"
+
+  /** Callers must coordinate so that only one call to an Instance (onany sequential Operation) may be outstanding at once.
+   */
+  // -beg- preserve=no 40458F4202AA var33F0EC2002BC "SEQUENTIAL"
+  public static final int SEQUENTIAL = 1;
+  // -end- 40458F4202AA var33F0EC2002BC "SEQUENTIAL"
+
+  /** Multiple calls from concurrent threads may occur simultaneouslyto one Instance (on any guarded Operation), but only one isallowed to commence.
+   */
+  // -beg- preserve=no 40458F4A030F var33F0EC2002BC "GUARDED"
+  public static final int GUARDED = 2;
+  // -end- 40458F4A030F var33F0EC2002BC "GUARDED"
+
+  /** Multiple calls from concurrent threads may occur simultaneouslyto one Instance (on any concurrent Operation).
+   */
+  // -beg- preserve=no 40458F550347 var33F0EC2002BC "CONCURRENT"
+  public static final int CONCURRENT = 3;
+  // -end- 40458F550347 var33F0EC2002BC "CONCURRENT"
 
   // declare/define something only in the code
   // please fill in/modify the following section
