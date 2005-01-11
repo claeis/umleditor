72eb536ee8647dbe78312f17d44f79cf3e9f94e4
claeis <claeis@users.noreply.github.com>
2005-01-11 08:15:42 +0000

    - modified to use generic call to ili2c by passing config obj - cejava tags removed; no longer generated by rose
    git-svn-id: file:///C/usr/ce/projects/sf/ili2c/gitmigration/umleditor.svn/umleditor/trunk@176 22a1f05a-e80c-4436-89a7-166f12dbcbdd

--- src/ch/ehi/umleditor/interlis/modelcheck/CheckModel.java
+++ src/ch/ehi/umleditor/interlis/modelcheck/CheckModel.java
@@ -1,58 +1,39 @@
-// Copyright (c) 2002, Eisenhut Informatik
-// All rights reserved.
-// $Date: 2003-12-23 10:40:33 $
-// $Revision: 1.1.1.1 $
-//
-
-// -beg- preserve=no 3CDA447803C0 package "CheckModel"
+/* This file is part of the UML/INTERLIS-Editor.
+ * For more information, please see <http://www.umleditor.org/>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package ch.ehi.umleditor.interlis.modelcheck;
-// -end- 3CDA447803C0 package "CheckModel"
-
-// -beg- preserve=no 3CDA447803C0 autoimport "CheckModel"
-
-// -end- 3CDA447803C0 autoimport "CheckModel"
-
-// import declarations
-// please fill in/modify the following section
-// -beg- preserve=yes 3CDA447803C0 import "CheckModel"
 import ch.ehi.umleditor.interlis.iliexport.TransferFromUmlMetamodel;
-// -end- 3CDA447803C0 import "CheckModel"
+import ch.interlis.ili2c.config.Configuration;
+import ch.interlis.ili2c.config.GenerateOutputKind;
 
 public class CheckModel
 {
-  // declare/define something only in the code
-  // please fill in/modify the following section
-  // -beg- preserve=no 3CDA447803C0 detail_begin "CheckModel"
-
-  // -end- 3CDA447803C0 detail_begin "CheckModel"
-
-  // -beg- preserve=no 3CDA448E01A5 head3CDA447803C0 "checkAll"
   public static void checkAll()
-  // -end- 3CDA448E01A5 head3CDA447803C0 "checkAll"
-    // declare any checked exceptions
-    // please fill in/modify the following section
-    // -beg- preserve=no 3CDA448E01A5 throws3CDA447803C0 "checkAll"
-
-    // -end- 3CDA448E01A5 throws3CDA447803C0 "checkAll"
     {
-    // please fill in/modify the following section
-    // -beg- preserve=yes 3CDA448E01A5 body3CDA447803C0 "checkAll"
     TransferFromUmlMetamodel writer=new TransferFromUmlMetamodel();
     try{
-       writer.setCheckModel(true);
-       writer.visitNamespace(ch.ehi.umleditor.application.LauncherView.getInstance().getModel());
+		Configuration ili2cConfig=new Configuration();
+		ili2cConfig.setOutputKind(GenerateOutputKind.NOOUTPUT);
+       writer.runCompiler(ch.ehi.umleditor.application.LauncherView.getInstance().getModel(),ili2cConfig);
     }catch(java.io.IOException ex){
       ch.ehi.umleditor.application.LauncherView.getInstance().log(writer.getFuncDesc(),ex.getLocalizedMessage());
     }
     return;
-    // -end- 3CDA448E01A5 body3CDA447803C0 "checkAll"
     }
-
-  // declare/define something only in the code
-  // please fill in/modify the following section
-  // -beg- preserve=no 3CDA447803C0 detail_end "CheckModel"
-
-  // -end- 3CDA447803C0 detail_end "CheckModel"
 
 }
 
