3cac60bdb4ab4d1ee235c0bfedf6ea247492eeb9
ivan <alejandrolistc@hotmail.com>
2017-11-02 10:35:46 -0500

    Don't save changes with handle escape key

--- src/ch/ehi/umleditor/application/ClassDefDialog.java
+++ src/ch/ehi/umleditor/application/ClassDefDialog.java
@@ -168,7 +168,7 @@ public class ClassDefDialog extends BaseDialog implements ListMenuChoice{
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				connEtoC1(arg0);
+				
 				dispose();
 			}
 		 };
