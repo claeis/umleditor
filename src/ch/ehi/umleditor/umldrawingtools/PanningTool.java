2e4c29a26f87d3cdd24ebebcd89093a8ca84a40b
alelizzt <alejandrolistc@hotmail.com>
2018-04-10 11:13:08 -0500

    Set hand cursor on panning tool

--- src/ch/ehi/umleditor/umldrawingtools/PanningTool.java
+++ src/ch/ehi/umleditor/umldrawingtools/PanningTool.java
@@ -1,13 +1,32 @@
 package ch.ehi.umleditor.umldrawingtools;
 
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import CH.ifa.draw.framework.DrawingEditor;
 import CH.ifa.draw.standard.AbstractTool;
 
-public class PanningTool extends AbstractTool {
+public class PanningTool extends AbstractTool implements ActionListener{
 
 	public PanningTool(DrawingEditor newDrawingEditor) {
 		super(newDrawingEditor);
+		
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+		
+	}
+	public void activate() {
+		super.activate();
+		if (isUsable()) {
+			view().setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		}
+	}
+
+	
 
 }
