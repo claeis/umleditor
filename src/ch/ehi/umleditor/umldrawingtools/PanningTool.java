56a982b328cc37c52d67f25b1bc0b505baef5553
alelizzt <alejandrolistc@hotmail.com>
2018-04-10 10:10:02 -0500

    Add icon to Toolbar

---
+++ src/ch/ehi/umleditor/umldrawingtools/PanningTool.java
@@ -0,0 +1,13 @@
+package ch.ehi.umleditor.umldrawingtools;
+
+import CH.ifa.draw.framework.DrawingEditor;
+import CH.ifa.draw.standard.AbstractTool;
+
+public class PanningTool extends AbstractTool {
+
+	public PanningTool(DrawingEditor newDrawingEditor) {
+		super(newDrawingEditor);
+		// TODO Auto-generated constructor stub
+	}
+
+}
