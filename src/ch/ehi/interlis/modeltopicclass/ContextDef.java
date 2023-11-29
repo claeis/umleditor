c2484fe84fd238153051ea443f6fac1a1e40820f
Patrick Ackermann <patrick.ackermann@geowerkstatt.ch>
2023-11-29 15:45:17 +0100

    Override unlinkAll and enumerateChildren

--- src/ch/ehi/interlis/modeltopicclass/ContextDef.java
+++ src/ch/ehi/interlis/modeltopicclass/ContextDef.java
@@ -1,5 +1,6 @@
 package ch.ehi.interlis.modeltopicclass;
 
+import ch.ehi.basics.tools.AbstractVisitor;
 import ch.ehi.basics.types.NlsString;
 import ch.ehi.interlis.IliSyntax;
 import ch.ehi.uml1_4.foundation.core.ModelElement;
@@ -10,6 +11,18 @@ import java.io.Serializable;
 public class ContextDef extends AbstractModelElement implements ModelElement, IliSyntax, Serializable {
 
     private  NlsString syntax = null;
+
+    @Override
+    public void unlinkAll() {
+        setSyntax(null);
+        super.unlinkAll();
+    }
+
+    @Override
+    public void enumerateChildren(AbstractVisitor visitor) {
+        visitor.visit(getSyntax());
+        super.enumerateChildren(visitor);
+    }
 
     @Override
     public NlsString getSyntax() {
